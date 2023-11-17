4c08b94614b7524fd12e2fadbc5f1d4bf5ab856e
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2023-11-17 19:34:21 +0900

    Title: 4번의 크기 비교, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231117/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,23 @@
+a = int(input())
+num = input().split()
+b = int(num[0])
+c = int(num[1])
+d = int(num[2])
+e = int(num[3])
+
+if a > b:
+    print(1)
+else:
+    print(0)
+if a > c:
+    print(1)
+else:
+    print(0)
+if a > d:
+    print(1)
+else:
+    print(0)
+if a > e:
+    print(1)
+else:
+    print(0)
