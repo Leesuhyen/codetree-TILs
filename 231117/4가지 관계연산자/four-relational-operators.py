6abcd9368dcf1ed86d6145c3c78ced5321e54df8
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2023-11-17 19:30:35 +0900

    Title: 4가지 관계연산자, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231117/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,20 @@
+num = input().split()
+a = int(num[0])
+b = int(num[1])
+
+if a >= b:
+    print(1)
+else:
+    print(0)
+if a > b:
+    print(1)
+else:
+    print(0)
+if b >= a:
+    print(1)
+else:
+    print(0)
+if b > a:
+    print(1)
+else:
+    print(0)
