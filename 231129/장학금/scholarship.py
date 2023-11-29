47818bf292065c2cee865c8c36c8ec25eea9f798
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2023-11-29 17:09:43 +0900

    Title: 장학금, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231129/장학금/scholarship.py
@@ -0,0 +1,11 @@
+score = input().split()
+middle = int(score[0])
+end = int(score[1])
+
+if middle >= 90:
+    if end >= 95:
+        print(100000)
+    elif end >= 90:
+        print(50000)
+else:
+    print(0)
