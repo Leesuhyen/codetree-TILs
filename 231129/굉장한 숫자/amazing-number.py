5780ab425119bf5f50f78e3c64ab546641174a1a
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2023-11-29 16:48:32 +0900

    Title: 굉장한 숫자, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231129/굉장한 숫자/amazing-number.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+'''
+if n % 3 == 0 and n // 2 != 0:
+    print("true")
+elif n % 5 == 0 and n // 2 != 0:
+    print("true")
+else:
+    print("false")
+'''
+if n % 3 == 0 and n // 2 != 0 or n % 5 == 0 and n // 2 != 0:
+    print("true")
+else:
+    print("false")
