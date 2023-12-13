944c4897f1fc74bc6d8ab42953f0bb94fac7c79a
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2023-12-13 18:18:16 +0900

    Title: 높은 수에서 낮은 수까지, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231213/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 231213/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -3,7 +3,7 @@ a = int(num[0])
 b = int(num[1])
 
 if a > b:
-    for i in range(a, b + 1, -1):
+    for i in range(a, b - 1, -1):
         print(i, end=' ')
 else:
     for i in range(b, a - 1, -1):
