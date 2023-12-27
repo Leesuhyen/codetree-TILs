976c83358a7735cf18e1c52dadddde22ebeff030
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2023-12-27 17:48:05 +0900

    Title: 짝수만 출력 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231227/짝수만 출력 2/output-only-even-2.py
@@ -0,0 +1,24 @@
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+t = a + 1
+'''
+if a < b:
+    if a % 2 == 0:
+        while a <= b:
+            print(b, end=' ')
+            b -= 2
+    else:
+        while t <= b:
+            print(b, end=' ')
+            b -= 2
+else:
+    if a % 2 == 0:
+        while b <= a:
+            print(a, end=' ')
+            a -= 2
+    else: 
+        while t <= b:
+            print(b, end=' ')
+            b -= 2        
+'''
