3fbe239e2596f72d002142c374092f8d9942d29f
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2024-01-17 16:59:19 +0900

    Title: 별표 출력하기 3, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240117/별표 출력하기 3/print-star-3.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    for k in range(i):
+        print('  ', end='')
+
+    for k in range((2 * n) - (2 * i) - 1):
+        print('*', end=' ')
+    print()
+'''
+    for k in range(n - i):
+        print(' ', end='')
+    print()
+'''
