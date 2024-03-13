86a987040a3e2e58e54df88629925bd5a04dfbb7
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2024-03-13 19:39:15 +0900

    Title: 좌우반전시킨 정사각형, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240313/좌우반전시킨 정사각형/left-right-inverted-square.py
+++ 240313/좌우반전시킨 정사각형/left-right-inverted-square.py
@@ -4,3 +4,10 @@ for i in range(n):
     for j in range(n):
         print(n - j + n*i - i*j, end=' ')
     print()
+
+'''
+for i in range(1, n+1):
+    for j in range(n):
+        print((n-j)*i, end=' ')
+    print()
+'''
