e9e54f2495751e8d9eca7eb4d5947bf0949e819a
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2024-03-27 19:34:35 +0900

    Title: 구구단 만들기 2, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/구구단 만들기 2/print-multiplication-table-2.py
+++ 240327/구구단 만들기 2/print-multiplication-table-2.py
@@ -2,7 +2,7 @@ n = input().split()
 a = int(n[0])
 b = int(n[1])
 
-for i in range(a, 9, 2):
+for i in range(2, 9, 2):
     for j in range(b, a - 1, - 1):
         print(f"{j} * {i} = {j * i}", end=' ')
         if j != a:
