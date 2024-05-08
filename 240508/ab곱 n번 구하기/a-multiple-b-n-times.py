1184cd76f575e71711ff856d66d3e04dd03ceb32
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2024-05-08 19:17:17 +0900

    Title: ab곱 n번 구하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240508/ab곱 n번 구하기/a-multiple-b-n-times.py
+++ 240508/ab곱 n번 구하기/a-multiple-b-n-times.py
@@ -1,12 +1,12 @@
 n = int(input())
 
-
-p = 0
-
 for i in range(n):
+    p = 1    
     num = input().split()
     a = int(num[0])
     b = int(num[1])
     for j in range(a, b + 1):
-        p += j
-    print()
+        p *= j
+    print(p)
+
+print()
