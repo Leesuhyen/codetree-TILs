c0c3b58bb800a2be102115a8e61c0108d2fcb1b8
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2024-08-14 19:58:49 +0900

    Title: 코로나 메뉴얼2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240814/코로나 메뉴얼2/covid-manual2.py
@@ -0,0 +1,19 @@
+st = [0] * 5
+for i in range(3):
+    num = input().split()
+    num[1] = int(num[1])
+    if num[0] == 'Y':
+        if num[1] > 36:
+            st[0] += 1
+        else:
+            st[2] += 1
+    else:
+        if num[1] > 36:
+            st[1] += 1
+        else:
+            st[3] += 1
+
+if st[0] >= 2:
+    sr = 'E'
+for i in range(1):
+    print(f"{st[0]} {st[1]} {st[2]} {st[3]} {sr}")
