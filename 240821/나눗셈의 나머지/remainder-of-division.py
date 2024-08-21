b0fa7d98219553b58d43a5e6069132ec1ae553a8
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2024-08-21 19:16:58 +0900

    Title: 나눗셈의 나머지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240821/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,12 @@
+num = list(map(int,input().split()))
+cnt_list = [0] * 5
+n = 0
+s = 0
+while num[0] >= 1:
+    num[0] = num[0] // num[1]
+    n = num[0] % num[1]
+    cnt_list[n] += 1
+
+for elem in cnt_list:
+    s += elem * elem
+print(s)
