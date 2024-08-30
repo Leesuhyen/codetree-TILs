a34d036dd7eebe7e5de1ab75cedfa539818c236f
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2024-08-30 16:04:50 +0900

    Title: 나눗셈의 나머지, Time: 200ms, Memory: 24MB, Status: Passed - CodeTree

--- 240830/나눗셈의 나머지/remainder-of-division.py
+++ 240830/나눗셈의 나머지/remainder-of-division.py
@@ -2,11 +2,13 @@ num = list(map(int,input().split()))
 cnt_num = [0] * 6
 n = 0
 s = 0
-while num[0] >= 1:
+while num[0] > 1:
     n = num[0] % num[1]
     num[0] = num[0] // num[1]
+
     if n >= len(cnt_num):
         cnt_num.extend([0] * (n - len(cnt_num) + 1))
+
     cnt_num[n] += 1
 for elem in cnt_num:
     s += elem * elem
