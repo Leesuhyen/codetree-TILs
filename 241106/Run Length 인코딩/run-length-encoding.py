c4aac706b9df5a278d5ee7381ad61fdde331b965
Leesuhyen <75783906+Leesuhyen@users.noreply.github.com>
2024-11-06 19:34:32 +0900

    Title: Run Length 인코딩, Time: 43ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241106/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,20 @@
+n = input()
+txt = '0'
+cnt = 0
+num =[]
+for i in range(len(n)):
+    if txt =='0':
+        txt = n[i]
+        cnt += 1
+        num.append(txt)
+    elif txt ==n[i]:
+        cnt += 1
+    else:
+        num.append(cnt)
+        cnt = 1
+        txt = n[i]
+        num.append(txt)
+num.append(cnt)
+print(len(num))
+for elem in num:
+    print(elem, end='')
